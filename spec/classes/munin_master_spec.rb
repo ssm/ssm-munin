1bf15d387eff73fffd49a5da7a92fdac68ab913c
Matthias Schmitz <matthias@sigxcpu.org>
2013-07-27 18:45:29 +0200

    Add rspec test for munin.conf template

--- spec/classes/munin_master_spec.rb
+++ spec/classes/munin_master_spec.rb
@@ -4,4 +4,18 @@ describe 'munin::master', :type => 'class' do
   it {
     should contain_package('munin')
   }
+
+  it do
+    should contain_file('/etc/munin/munin.conf')\
+      .with_content(/^graph_strategy cgi$/)
+  end
+
+  context 'with graph_strategy => cron' do
+    let(:params) { {:graph_strategy => 'cron'} }
+
+    it do
+      should contain_file('/etc/munin/munin.conf')\
+        .with_content(/^graph_strategy cron$/)
+    end
+  end
 end
