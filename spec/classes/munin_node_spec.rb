1627221bf78a0f54e31e265f49daf56d253e002c
Stig Sandbeck Mathisen <ssm@fnord.no>
2014-01-31 10:57:52 +0100

    update test

--- spec/classes/munin_node_spec.rb
+++ spec/classes/munin_node_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe 'munin::node', :type => 'class' do
-  it {
-    should contain_package('munin-node')
-  }
+  context 'default' do
+    let(:facts) do
+      { :fqdn => 'example.com' }
+    end
+
+    it {
+      should contain_package('munin-node')
+      should contain_service('munin-node')
+      should contain_file('/etc/munin/munin-node.conf')
+    }
+  end
 end
