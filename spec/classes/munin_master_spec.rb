d9ea03f85ebfaf8fbac75f67f388469ecaa3c59e
Stig Sandbeck Mathisen <ssm@redpill-linpro.com>
2014-07-14 16:13:36 +0200

    Update tests

--- spec/classes/munin_master_spec.rb
+++ spec/classes/munin_master_spec.rb
@@ -1,22 +1,46 @@
 require 'spec_helper'
 
 describe 'munin::master', :type => 'class' do
-  include_context :Debian
-  it {
-    should contain_package('munin')
-  }
-
-  it do
-    should contain_file('/etc/munin/munin.conf')\
-      .with_content(/^graph_strategy cgi$/)
+  context 'default' do
+    include_context :Debian
+    it { should compile }
+    it {
+      should contain_package('munin')
+      should contain_file('/etc/munin/munin.conf')\
+        .with_content(/^graph_strategy cgi$/)
+    }
   end
 
+
   context 'with graph_strategy => cron' do
+    include_context :Debian
     let(:params) { {:graph_strategy => 'cron'} }
 
-    it do
-      should contain_file('/etc/munin/munin.conf')\
+    it { should compile }
+    it {
+      should contain_file('/etc/munin/munin.conf')
         .with_content(/^graph_strategy cron$/)
-    end
+    }
+  end
+
+  context 'with tls => enabled' do
+    include_context :Debian
+    let(:params) {
+      {
+        :tls => 'enabled',
+        :tls_certificate => '/path/to/certificate.pem',
+        :tls_private_key => '/path/to/key.pem',
+        :tls_verify_certificate => 'yes',
+      }
+    }
+
+    it { should compile }
+    it {
+      should contain_file('/etc/munin/munin.conf')
+        .with_content(/tls = enabled/)
+        .with_content(/tls_certificate = \/path\/to\/certificate\.pem/)
+        .with_content(/tls_private_key = \/path\/to\/key\.pem/)
+        .with_content(/tls_verify_certificate = yes/)
+    }
   end
 end
