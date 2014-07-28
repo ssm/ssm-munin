1d9e40a8846ebe99affcd9242987e8939df78f02
Stig Sandbeck Mathisen <ssm@debian.org>
2014-07-28 14:11:47 +0200

    tests: use default_facts to clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,35 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+RSpec.configure do |c|
+  c.default_facts = {
+    :operatingsystem => 'Debian',
+    :osfamily        => 'Debian',
+    :fqdn            => 'testnode.example.com',
+  }
+end
+
+
 shared_context :unsupported do
-  let(:facts) do
-    {
-      :osfamily => 'Unsupported',
-      :fqdn     => 'testnode.example.com',
-    }
-  end
+  let(:facts) { { :osfamily => 'Unsupported', } }
 end
 
 shared_context :Debian do
-  let(:facts) do
-    {
-      :osfamily => 'Debian',
-      :fqdn     => 'testnode.example.com',
-    }
-  end
+end
+
+shared_context :Ubuntu do
+  let(:facts) { { :Operatingsystem => 'Ubuntu', } }
+end
+
+shared_context :CentOS do
+  let(:facts) { { :operatingsystem => 'CentOS', :osfamily => 'RedHat', } }
 end
 
 shared_context :RedHat do
-  let(:facts) do
-    {
-      :osfamily => 'RedHat',
-      :fqdn     => 'testnode.example.com',
-    }
-  end
+  let(:facts) { { :osfamily => 'RedHat', } }
 end
 
 shared_context :SmartOS do
-  let(:facts) do
-    {
-      :operatingsystem => 'SmartOS',
-      :osfamily        => 'Solaris',
-      :fqdn            => 'testnode.example.com',
-    }
-  end
+  let(:facts) { { :operatingsystem => 'SmartOS', :osfamily => 'Solaris', } }
 end
 
