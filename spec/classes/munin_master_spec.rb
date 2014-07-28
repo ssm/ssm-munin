2dfca309778d834b5ade93e19e2a9e4293c7701b
Stig Sandbeck Mathisen <ssm@debian.org>
2014-07-28 14:29:10 +0200

    tests: add more test cases

--- spec/classes/munin_master_spec.rb
+++ spec/classes/munin_master_spec.rb
@@ -1,30 +1,60 @@
 require 'spec_helper'
 
-describe 'munin::master', :type => 'class' do
-  context 'default' do
-    include_context :Debian
-    it { should compile }
+describe 'munin::master' do
+
+  [ :CentOS, :Debian, :RedHat, :Ubuntu ].each do |sc|
+    context "Check for supported operatingsystem #{sc}" do
+      include_context sc
+      it { should compile }
+      it { should contain_class('munin::master') }
+      it {
+        should contain_package('munin')
+        should contain_file('/etc/munin/munin.conf')
+        should contain_file('/etc/munin/munin-conf.d')
+          .with_ensure('directory')
+      }
+    end
+  end
+
+  [ :SmartOS ].each do |sc|
+    context "Check for supported operatingsystem #{sc}" do
+      include_context sc
+      it { should compile }
+      it { should contain_class('munin::master') }
+      it {
+        should contain_package('munin')
+        should contain_file('/opt/local/etc/munin/munin.conf')
+        should contain_file('/opt/local/etc/munin/munin-conf.d')
+          .with_ensure('directory')
+      }
+    end
+  end
+
+  context 'with default params' do
     it {
-      should contain_package('munin')
       should contain_file('/etc/munin/munin.conf')
-        .with_content(/^graph_strategy cgi$/)
+        .with_content(/graph_strategy\s+cgi/)
+        .with_content(/html_strategy\s+cgi/)
     }
   end
 
-
-  context 'with graph_strategy => cron' do
-    include_context :Debian
-    let(:params) { {:graph_strategy => 'cron'} }
-
-    it { should compile }
+  context 'with html_strategy => cron' do
+    let (:params) { { :html_strategy => 'cron' } }
     it {
       should contain_file('/etc/munin/munin.conf')
-        .with_content(/^graph_strategy cron$/)
+        .with_content(/html_strategy\s+cron/)
+    }
+  end
+
+  context 'with graph_strategy => cron' do
+    let (:params) { { :graph_strategy => 'cron' } }
+    it {
+      should contain_file('/etc/munin/munin.conf')
+        .with_content(/graph_strategy\s+cron/)
     }
   end
 
   context 'with tls => enabled' do
-    include_context :Debian
     let(:params) {
       {
         :tls => 'enabled',
@@ -43,4 +73,5 @@ describe 'munin::master', :type => 'class' do
         .with_content(/tls_verify_certificate = yes/)
     }
   end
+
 end
