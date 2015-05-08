791eb682c361089d9f0c4cd2de3ebc3df7c98ae0
Stig Sandbeck Mathisen <ssm@redpill-linpro.com>
2015-05-08 17:27:35 +0200

    tests: stop on first failed test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
+RSpec.configure do |c|
+  c.fail_fast = true
+end
+
 shared_context :unsupported do
   let(:facts) { { osfamily: 'Unsupported' } }
 end
